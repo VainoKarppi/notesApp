324d061216f4a691048784b425bb82f5b7f7fee4
Väinö Karppi <vaino.karppi@gmail.com>
2023-09-11 13:46:08 +0300

    ADDED: Account class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("init")
+Accounts = []
+class Account:
+    def __init__(self, name, password, uuid = None, salt = None):
+        from uuid import uuid4
+        from random import randint
+        self.uuid = str(uuid4()) if uuid is None else uuid
+        self.name = name
+        self.salt = str(randint(1000000,9999999)) if salt is None else salt
+        self.password = password if IsMD5hash(password) else str(ComputeMD5hash(password,self.salt))
+        
+
