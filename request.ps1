836873cc1b9d9f3844bf7aefd1b4c4db951ea8d9
Väinö Karppi <vaino.karppi@gmail.com>
2023-11-14 22:44:31 +0200

    UPDATED: request.ps1 tester with query

--- request.ps1
+++ request.ps1
@@ -1,9 +1,17 @@
 $username = "admin"
 $password = "admin"
-$uri = "http://localhost:8000/test"
+$url = "http://127.0.0.1:8000/notes"
 $headers = @{
     Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($username):$($password)"))
 }
-Invoke-WebRequest -Uri $uri -Headers $headers
+Invoke-WebRequest -Uri $url -Headers $headers
+
+
+# CREATE TEST NOTE FIRST WITH SUBJECT: test
+$body = @{
+    "subject"="test"
+}
+Invoke-WebRequest -Uri "http://localhost:8000/note" -Method Get -Headers $headers -Body $body
+
 
 
